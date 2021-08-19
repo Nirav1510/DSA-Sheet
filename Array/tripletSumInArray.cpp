4c1afad7250d3a881bb9037f4e74abfbd8ed7ac2
Nirav <gusainirav1@gmail.com>
2021-08-19 12:59:15 +0530

    medium

--- Array/tripletSumInArray.cpp
+++ Array/tripletSumInArray.cpp
@@ -1,23 +1,28 @@
 bool find3Numbers(int A[], int n, int X)
+{
+    //Your Code Here
+    sort(A, A + n);
+
+    for (int i = 0; i < n - 1; i++)
     {
-        //Your Code Here
-        sort(A,A+n);
-    
-        for(int i=0;i<n-1;i++){
-            int j=i+1,k=n-1;
-            while(j<k){
-                int sum=A[i]+A[j]+A[k];
-                
-                if(sum==X){
-                    return true;
-                }
-                else if(sum<X){
-                    j++;
-                }
-                else{
-                    k--;
-                }
+        int j = i + 1, k = n - 1;
+        while (j < k)
+        {
+            int sum = A[i] + A[j] + A[k];
+
+            if (sum == X)
+            {
+                return true;
+            }
+            else if (sum < X)
+            {
+                j++;
+            }
+            else
+            {
+                k--;
             }
         }
-        return false;
     }
+    return false;
+}
